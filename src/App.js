045b5d04e58271bd5ac1d89ccdcbed6dd478de56
ClothAtTheFair <bradleybsf12@gmail.com>
2020-02-06 23:52:12 -0500

    Started working with API calls

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ import Chart from 'chart.js';
 export default class App extends Component {
   chartRef = React.createRef();
 
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
   componentDidMount(){
+    fetch("https://www.thebluealliance.com/api/v3/team/frc3490")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
     const myChartRef = this.chartRef.current.getContext("2d");
 
     new Chart(myChartRef, {
@@ -38,9 +64,11 @@ export default class App extends Component {
   }
 
   render() {
+    const { error, isLoaded, items} = this.state;
     return(
       <div>
         <canvas id="myChart" ref={this.chartRef}></canvas>
+        <h1>{this.state.error? "True" : "False"}</h1>
       </div>
     )
   }
